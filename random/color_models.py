c609ed6395ae1a043046108a1e216bfdf89b177e
Yeoh Joer <yeoh.joer@outlook.com>
2024-04-30 04:28:24 +0800

    feat: convert pixels from rgb to hsi format

---
+++ random/color_models.py
@@ -0,0 +1,26 @@
+# %%
+import numpy as np
+
+
+# %%
+def rgb_to_hsi(pixel: list | tuple):
+    R, G, B = pixel
+
+    numerator = ((R - G) + (R - B)) / 2
+    denominator = np.sqrt((R - G) ** 2 + (R - B) * (G - B))
+
+    H = np.rad2deg(np.arccos(numerator / denominator))
+
+    if B > G:
+        H = 360 - H
+
+    S = 1 - (3 / (R + G + B)) * np.min(pixel)
+    I = (R + G + B) / 3
+
+    return H, S, I
+
+
+# %%
+rgb_to_hsi([50 / 255, 10 / 255, 200 / 255])
+
+# %%
