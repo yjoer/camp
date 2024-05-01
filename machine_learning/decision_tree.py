26cbc0ac2ff6845a3edd68a9f27be54f31b3850f
Yeoh Joer <yeoh.joer@outlook.com>
2024-05-02 05:49:16 +0800

    feat: visualize the structures for different hyperparameters

--- machine_learning/decision_tree.py
+++ machine_learning/decision_tree.py
@@ -20,8 +20,11 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=12345,
 )
 
+# %% [markdown]
+# **Default Parameters**
+
 # %%
-dt = DecisionTreeClassifier()
+dt = DecisionTreeClassifier(random_state=12345)
 dt.fit(X_train, y_train)
 
 # %%
@@ -30,6 +33,58 @@ accuracy_score(y_test, y_pred)
 
 # %%
 fig = plt.figure(figsize=(10, 8))
+ax = fig.gca()
+
+plot_tree(
+    dt,
+    feature_names=iris.feature_names,
+    class_names=iris.target_names,
+    filled=True,
+    rounded=True,
+    ax=ax,
+)
+
+plt.show()
+
+# %% [markdown]
+# **Max Depth = 3**
+
+# %%
+dt = DecisionTreeClassifier(max_depth=3, random_state=12345)
+dt.fit(X_train, y_train)
+
+# %%
+y_pred = dt.predict(X_test)
+accuracy_score(y_test, y_pred)
+
+# %%
+fig = plt.figure(figsize=(6, 6))
+ax = fig.gca()
+
+plot_tree(
+    dt,
+    feature_names=iris.feature_names,
+    class_names=iris.target_names,
+    filled=True,
+    rounded=True,
+    ax=ax,
+)
+
+plt.show()
+
+# %% [markdown]
+# **Min Samples Leaf = 3**
+
+# %%
+dt = DecisionTreeClassifier(min_samples_leaf=3, random_state=12345)
+dt.fit(X_train, y_train)
+
+# %%
+y_pred = dt.predict(X_test)
+accuracy_score(y_test, y_pred)
+
+# %%
+fig = plt.figure(figsize=(7.2, 7.2))
 ax = fig.gca()
 
 plot_tree(
