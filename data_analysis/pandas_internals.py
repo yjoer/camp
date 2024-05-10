2d260396395903151497d0b3c075c1549ff74712
Yeoh Joer <yeoh.joer@outlook.com>
2024-05-11 06:08:13 +0800

    feat: introspect internal blocks with and without copy-on-write enabled

---
+++ data_analysis/pandas_internals.py
@@ -0,0 +1,36 @@
+# %%
+import pandas as pd
+
+# %%
+df = pd.DataFrame(
+    {
+        "A": [1, 2, 3],
+        "B": [4, 5, 6],
+        "C": [1.0, 2.0, 3.0],
+        "D": [4.0, 5.0, 6.0],
+        "E": ["A", "B", "C"],
+        "F": ["D", "E", "F"],
+    },
+)
+
+# %%
+df._mgr
+
+# %%
+with pd.option_context("mode.copy_on_write", False):
+    df_copy = df.copy()
+    vw = df_copy[:]
+    vw.iloc[0, 0] = 10
+
+    print(df_copy._mgr)
+    print(vw._mgr)
+
+# %%
+with pd.option_context("mode.copy_on_write", True):
+    vw = df[:]
+    vw.iloc[0, 0] = 10
+
+    print(df._mgr)
+    print(vw._mgr)
+
+# %%
