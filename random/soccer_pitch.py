f6054bce69238cf2095f499e36641438990be3ec
Yeoh Joer <yeoh.joer@outlook.com>
2024-12-22 10:48:12 +0800

    feat: wip

--- random/soccer_pitch.py
+++ random/soccer_pitch.py
@@ -1,8 +1,31 @@
 # %%
+from typing import Optional
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sp
+from ellipse import LsqEllipse
+from IPython.display import display
+from matplotlib.patches import Ellipse
+
 from solutions.sports.yolo_v8_calibration_pipeline import SoccerPitch
+from solutions.sports.yolo_v8_calibration_pipeline import (
+    find_ellipse_tangent_intersections,
+)
+
+# %matplotlib inline
+# %config InlineBackend.figure_formats = ['retina']
 
 # %load_ext autoreload
 # %autoreload 2
+
+# %% language="html"
+#
+# <style>
+#     mjx-assistive-mml {
+#         height: 100%;
+#     }
+# </style>
 
 # %%
 pitch = SoccerPitch()
@@ -11,4 +34,249 @@ pitch = SoccerPitch()
 fig = pitch.plot()
 fig.show()
 
+
 # %%
+def make_ellipse(
+    n_samples: int,
+    width: int,
+    height: int,
+    theta: int,
+    noise: float,
+    random_state: Optional[int],
+):
+    t = np.linspace(0, 2 * np.pi, n_samples)
+    x = width * np.cos(t)
+    y = height * np.sin(t)
+    theta = np.radians(theta)
+
+    rng = np.random.default_rng(seed=random_state)
+    gaussian_noise = rng.normal(size=(len(t), 2))
+
+    rotation_matrix = np.array(
+        [
+            [np.cos(theta), np.sin(theta)],
+            [-np.sin(theta), np.cos(theta)],
+        ]
+    )
+
+    points = (np.vstack([x, y]).T @ rotation_matrix) + (noise * gaussian_noise)
+
+    return points[:, 0], points[:, 1]
+
+
+# %%
+width, height, theta = 24, 16, 30
+x, y = make_ellipse(250, width, height, theta, noise=3, random_state=26)
+
+reg = LsqEllipse().fit(np.array([x, y]).T)
+center, width, height, theta = reg.as_parameters()
+
+ellipse_kwargs = {
+    "xy": center,
+    "width": width * 2,
+    "height": height * 2,
+    "angle": np.degrees(theta),
+    "edgecolor": "orange",
+    "fc": "None",
+    "label": "Least Squares Fit",
+}
+
+print(center, width, height, np.degrees(theta))
+
+plt.scatter(x, y, label="Points")
+plt.gca().add_patch(Ellipse(**ellipse_kwargs))
+plt.legend()
+plt.show()
+
+# %%
+symbols = ["a", "b", "c", "d", "e", "f"]
+grid = np.empty((len(symbols), len(symbols)), dtype="U2")
+
+for i, v1 in enumerate(symbols):
+    for j, v2 in enumerate(symbols):
+        grid[i][j] = f"{v1}{v2}"
+
+grid
+
+# %% [markdown]
+# $(a + b + c + d + e + f)^2$ \
+# $= a^2 + b^2 + c^2 + d^2 + e^2 + f^2$ \
+# $+ 2ab + 2ac + 2ad + 2ae + 2af$ \
+# $+ 2bc + 2bd + 2be + 2bf$ \
+# $+ 2cd + 2ce + 2cf$ \
+# $+ 2de + 2df$ \
+# $+ 2ef$
+
+# %%
+symbols = ["a", "b", "c", "d", "e", "f", "g", "h"]
+grid = np.empty((len(symbols), len(symbols)), dtype="U2")
+
+for i, v1 in enumerate(symbols):
+    for j, v2 in enumerate(symbols):
+        grid[i][j] = f"{v1}{v2}"
+
+grid
+
+# %% [markdown]
+# $(a + b + c + d + e + f + g + h)^2$ \
+# $= a^2 + b^2 + c^2 + d^2 + e^2 + f^2 + g^2 + h^2$ \
+# $+ 2ab + 2ac + 2ad + 2ae + 2af + 2ag + 2ah$ \
+# $+ 2bc + 2bd + 2be + 2bf + 2bg + 2bh$ \
+# $+ 2cd + 2ce + 2cf + 2cg + 2ch$ \
+# $+ 2de + 2df + 2dg + 2dh$ \
+# $+ 2ef + 2eg + 2eh$ \
+# $+ 2fg + 2fh$ \
+# $+ 2gh$
+
+# %% [markdown]
+# $$
+# \begin{flalign}
+# Ax^2 + Bxy + Cy^2 + Dx + Ey + F = 0 \tag{1} &&
+# \end{flalign}
+# $$
+#
+# $$
+# \begin{flalign}
+# y - y_1 &= m(x - x_1) \\
+# y &= y_1 + mx - mx_1 &&
+# \tag{2}
+# \end{flalign}
+# $$
+#
+# Substitute $(2)$ into $(1)$ to eliminate $y:$
+#
+# $Ax^2 + Bx(y_1 + mx - mx_1) + C(y_1 + mx - mx_1)^2 + Dx + E(y_1 + mx - mx_1) + F = 0$
+#
+# Expand:
+#
+# $$
+# \begin{flalign}
+# Ax^2 &+ Bmx^2 - Bmxx_1 + Bxy_1 \\
+# &+ Cm^2x^2 + Cm^2x_1^2 - 2Cm^2xx_1 + 2Cmxy_1 - 2Cmx_1y_1 + Cy_1^2 \\
+# &+ Dx + Emx - Emx_1 + Ey_1 + F = 0 &&
+# \tag{3}
+# \end{flalign}
+# $$
+#
+# Rearrange $(3)$ into $A'x^2 + B'x + C' = 0:$
+#
+# $A' = A + Bm + Cm^2$ \
+# $B' = -Bmx_1 + By_1 - 2Cm^2x_1 + 2Cmy_1 + D + Em$ \
+# $C' = Cm^2x_1^2 - 2Cmx_1y_1 + Cy_1^2 - Emx_1 + Ey_1 + F$
+
+# %% [markdown]
+# Discriminant at the tangent: $B'^2 - 4A'C' = 0$
+#
+# $$
+# \begin{flalign}
+# &B'^2 \\
+# &= B^2m^2x_1^2 + B^2y_1^2 + 4C^2m^4x_1^2 + 4C^2m^2y_1^2 + D^2 + E^2m^2 \\
+# &- 2B^2mx_1y_1 + 4BCm^3x_1^2 - 4BCm^2x_1y_1 - 2BDmx_1 - 2BEm^2x_1 \\
+# &- 4BCm^2x_1y_1 + 4BCmy_1^2 + 2BDy_1 + 2BEmy_1 \\
+# &- 8C^2m^3x_1y_1 - 4CDm^2x_1 - 4CEm^3x_1 \\
+# &+ 4CDmy_1 + 4CEm^2y_1 \\
+# &+ 2DEm &&
+# \end{flalign}
+# $$
+#
+# $$
+# \begin{flalign}
+# &4A'C' \\
+# &= 4ACm^2x_1^2 - 8ACmx_1y_1 + 4ACy_1^2 - 4AEmx_1 + 4AEy_1 + 4AF \\
+# &+ 4BCm^3x_1^2 - 8BCm^2x_1y_1 + 4BCmy_1^2 - 4BEm^2x_1 + 4BEmy_1 + 4BFm \\
+# &+ 4C^2m^4x_1^2 - 8C^2m^3x_1y_1 + 4C^2m^2y_1^2 - 4CEm^3x_1+ 4CEm^2y_1 + 4CFm^2 &&
+# \end{flalign}
+# $$
+#
+# $$
+# \begin{flalign}
+# &B'^2 - 4A'C' \\
+# &= B^2m^2x_1^2 + B^2y_1^2 + D^2 + E^2m^2 - 2B^2mx_1y_1 - 2BDmx_1 \\
+# &+ 2BEm^2x_1 + 2BDy_1 - 2BEmy_1 - 4CDm^2x_1 + 4CDmy_1 + 2DEm \\
+# &- 4ACm^2x_1^2 + 8ACmx_1y_1 - 4ACy_1^2 + 4AEmx_1 - 4AEy_1 - 4AF - 4BFm - 4CFm^2 &&
+# \tag{4}
+# \end{flalign}
+# $$
+#
+# Rearrange $(4)$ into $A''m^2 + B''m + C'' = 0:$
+#
+# $A'' = B^2x_1^2 + E^2 + 2BEx_1 - 4CDx_1 - 4ACx_1^2 - 4CF$ \
+# $B'' = -2B^2x_1y_1 - 2BDx_1 - 2BEy_1 + 4CDy_1 + 2DE + 8ACx_1y_1 + 4AEx_1 - 4BF$ \
+# $C'' = B^2y_1^2 + D^2 + 2BDy_1 - 4 ACy_1^2 - 4AEy_1 - 4AF$
+
+# %% [markdown]
+# Two Distinct Roots of Quadratic Equations:
+#
+# $$
+# \begin{flalign}
+# x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a} &&
+# \end{flalign}
+# $$
+#
+# Equal Roots of Quadratic Equations:
+#
+# $$
+# \begin{flalign}
+# \because \; &b^2 - 4ac = 0 \text{ and } \sqrt{0} = 0 \\
+# &x = \frac{-b}{2a} &&
+# \end{flalign}
+# $$
+
+# %%
+A, B, C, D, E, F = sp.symbols("A B C D E F")
+m, x, y, x1, y1 = sp.symbols("m x y x1 y1")
+
+# %%
+ellipse_eq = A * x**2 + B * x * y + C * y**2 + D * x + E * y + F
+line_eq = y1 + m * x - m * x1
+
+ellipse_eq_y = sp.expand(ellipse_eq.subs(y, line_eq))
+display(ellipse_eq_y)
+
+# %%
+A1 = ellipse_eq_y.coeff(x, 2)
+B1 = ellipse_eq_y.coeff(x, 1)
+C1 = ellipse_eq_y.coeff(x, 0)
+
+display(A1)
+display(B1)
+display(C1)
+
+# %%
+discriminant = sp.expand(B1**2 - 4 * A1 * C1)
+
+A2 = discriminant.coeff(m, 2)
+B2 = discriminant.coeff(m, 1)
+C2 = discriminant.coeff(m, 0)
+
+display(A2)
+display(B2)
+display(C2)
+
+# %%
+root_m = sp.simplify((-B2 + sp.sqrt(sp.factor(B2**2 - 4 * A2 * C2))) / (2 * A2))
+root_m
+
+# %%
+root_x = sp.factor(-B1 / (2 * A1))
+root_x
+
+# %%
+point = np.array([-5, 50])
+points = find_ellipse_tangent_intersections(reg.coefficients, point)
+
+plt.plot([point[0], points[0][0]], [point[1], points[0][1]], zorder=2)
+plt.plot([point[0], points[1][0]], [point[1], points[1][1]], zorder=2)
+
+plt.scatter(point[0], point[1], label="Point", zorder=3)
+
+plt.scatter(
+    [points[0][0], points[1][0]],
+    [points[0][1], points[1][1]],
+    label="Intersections",
+    zorder=3,
+)
+
+plt.gca().add_patch(Ellipse(**ellipse_kwargs))
+plt.legend()
+plt.show()
--- solutions/sports/yolo_v8_calibration_pipeline.py
+++ solutions/sports/yolo_v8_calibration_pipeline.py
@@ -1,7 +1,14 @@
+from typing import Generic
+from typing import Literal
+from typing import TypedDict
+from typing import TypeVar
+
 import numpy as np
 import plotly.graph_objects as go
 import torch
 import torchvision.transforms.v2.functional as tvf
+from ellipse import LsqEllipse
+from numpy.polynomial import polynomial as P
 
 
 def transforms(image, target):
@@ -40,6 +47,70 @@ def find_circle_tangent_intersections(
     return intersection_1, intersection_2
 
 
+# https://docs.python.org/3/whatsnew/3.12.html#pep-695-type-parameter-syntax
+T = TypeVar("T")
+
+
+class SoccerPitchKeypoints(TypedDict, Generic[T]):
+    center_mark: T
+    halfway_top_touch_line_mark: T
+    halfway_bottom_touch_line_mark: T
+    halfway_line_center_circle_top_mark: T
+    halfway_line_center_circle_bottom_mark: T
+    top_left_corner: T
+    top_right_corner: T
+    bottom_left_corner: T
+    bottom_right_corner: T
+    left_goal_top_left_post: T
+    left_goal_top_right_post: T
+    left_goal_bottom_left_post: T
+    left_goal_bottom_right_post: T
+    right_goal_top_left_post: T
+    right_goal_top_right_post: T
+    right_goal_bottom_left_post: T
+    right_goal_bottom_right_post: T
+    left_goal_area_top_left_corner: T
+    left_goal_area_top_right_corner: T
+    left_goal_area_bottom_left_corner: T
+    left_goal_area_bottom_right_corner: T
+    right_goal_area_top_left_corner: T
+    right_goal_area_top_right_corner: T
+    right_goal_area_bottom_left_corner: T
+    right_goal_area_bottom_right_corner: T
+    left_penalty_mark: T
+    right_penalty_mark: T
+    left_penalty_area_top_left_corner: T
+    left_penalty_area_top_right_corner: T
+    left_penalty_area_bottom_left_corner: T
+    left_penalty_area_bottom_right_corner: T
+    right_penalty_area_top_left_corner: T
+    right_penalty_area_top_right_corner: T
+    right_penalty_area_bottom_left_corner: T
+    right_penalty_area_bottom_right_corner: T
+    top_left_penalty_ark_mark: T
+    top_right_penalty_ark_mark: T
+    bottom_left_penalty_ark_mark: T
+    bottom_right_penalty_ark_mark: T
+    center_circle_top_left_corner: T
+    center_circle_top_right_corner: T
+    center_circle_bottom_left_corner: T
+    center_circle_bottom_right_corner: T
+    center_circle_left: T
+    center_circle_right: T
+    left_penalty_ark_mark: T
+    left_penalty_ark_right_mark: T
+    right_penalty_ark_mark: T
+    right_penalty_ark_left_mark: T
+    center_circle_top_left_tangent: T
+    center_circle_top_right_tangent: T
+    center_circle_bottom_left_tangent: T
+    center_circle_bottom_right_tangent: T
+    left_penalty_ark_top_tangent: T
+    left_penalty_ark_bottom_tangent: T
+    right_penalty_ark_top_tangent: T
+    right_penalty_ark_bottom_tangent: T
+
+
 class SoccerPitch:
     PITCH_LENGTH = 105.0
     PITCH_WIDTH = 68.0
@@ -69,7 +140,7 @@ class SoccerPitch:
         rkp_3 = [self.PITCH_LENGTH / 2 - self.PENALTY_AREA_LENGTH, -self.PENALTY_AREA_WIDTH / 2, 0]  # fmt: skip
         rkp_4 = [self.PITCH_LENGTH / 2 - self.PENALTY_AREA_LENGTH, self.PENALTY_AREA_WIDTH / 2, 0]  # fmt: skip
 
-        self.keypoints = {
+        self.keypoints: SoccerPitchKeypoints[np.ndarray[tuple[Literal[2]]]] = {
             "center_mark": np.array(
                 [0, 0, 0],
                 dtype=np.float32,
@@ -373,3 +444,321 @@ class SoccerPitch:
         )
 
         return fig
+
+
+KEYPOINTS_INDEX: SoccerPitchKeypoints[int] = {
+    "center_mark": 0,
+    "halfway_top_touch_line_mark": 1,
+    "halfway_bottom_touch_line_mark": 2,
+    "halfway_line_center_circle_top_mark": 3,
+    "halfway_line_center_circle_bottom_mark": 4,
+    "top_left_corner": 5,
+    "top_right_corner": 6,
+    "bottom_left_corner": 7,
+    "bottom_right_corner": 8,
+    "left_goal_top_left_post": 9,
+    "left_goal_top_right_post": 10,
+    "left_goal_bottom_left_post": 11,
+    "left_goal_bottom_right_post": 12,
+    "right_goal_top_left_post": 13,
+    "right_goal_top_right_post": 14,
+    "right_goal_bottom_left_post": 15,
+    "right_goal_bottom_right_post": 16,
+    "left_goal_area_top_left_corner": 17,
+    "left_goal_area_top_right_corner": 18,
+    "left_goal_area_bottom_left_corner": 19,
+    "left_goal_area_bottom_right_corner": 20,
+    "right_goal_area_top_left_corner": 21,
+    "right_goal_area_top_right_corner": 22,
+    "right_goal_area_bottom_left_corner": 23,
+    "right_goal_area_bottom_right_corner": 24,
+    "left_penalty_mark": 25,
+    "right_penalty_mark": 26,
+    "left_penalty_area_top_left_corner": 27,
+    "left_penalty_area_top_right_corner": 28,
+    "left_penalty_area_bottom_left_corner": 29,
+    "left_penalty_area_bottom_right_corner": 30,
+    "right_penalty_area_top_left_corner": 31,
+    "right_penalty_area_top_right_corner": 32,
+    "right_penalty_area_bottom_left_corner": 33,
+    "right_penalty_area_bottom_right_corner": 34,
+    "top_left_penalty_ark_mark": 35,
+    "top_right_penalty_ark_mark": 36,
+    "bottom_left_penalty_ark_mark": 37,
+    "bottom_right_penalty_ark_mark": 38,
+    "center_circle_top_left_corner": 39,
+    "center_circle_top_right_corner": 40,
+    "center_circle_bottom_left_corner": 41,
+    "center_circle_bottom_right_corner": 42,
+    "center_circle_left": 43,
+    "center_circle_right": 44,
+    "left_penalty_ark_mark": 45,
+    "left_penalty_ark_right_mark": 46,
+    "right_penalty_ark_mark": 47,
+    "right_penalty_ark_left_mark": 48,
+    "center_circle_top_left_tangent": 49,
+    "center_circle_top_right_tangent": 50,
+    "center_circle_bottom_left_tangent": 51,
+    "center_circle_bottom_right_tangent": 52,
+    "left_penalty_ark_top_tangent": 53,
+    "left_penalty_ark_bottom_tangent": 54,
+    "right_penalty_ark_top_tangent": 55,
+    "right_penalty_ark_bottom_tangent": 56,
+}
+
+LINE_INTERSECTIONS = {
+    0: ("Goal left crossbar", "Goal left post left "),
+    1: ("Goal left crossbar", "Goal left post right"),
+    2: ("Side line left", "Goal left post left "),
+    3: ("Side line left", "Goal left post right"),
+    4: ("Small rect. left main", "Small rect. left bottom"),
+    5: ("Small rect. left main", "Small rect. left top"),
+    6: ("Side line left", "Small rect. left bottom"),
+    7: ("Side line left", "Small rect. left top"),
+    8: ("Big rect. left main", "Big rect. left bottom"),
+    9: ("Big rect. left main", "Big rect. left top"),
+    10: ("Side line left", "Big rect. left bottom"),
+    11: ("Side line left", "Big rect. left top"),
+    12: ("Side line left", "Side line bottom"),
+    13: ("Side line left", "Side line top"),
+    14: ("Middle line", "Side line bottom"),
+    15: ("Middle line", "Side line top"),
+    16: ("Big rect. right main", "Big rect. right bottom"),
+    17: ("Big rect. right main", "Big rect. right top"),
+    18: ("Side line right", "Big rect. right bottom"),
+    19: ("Side line right", "Big rect. right top"),
+    20: ("Small rect. right main", "Small rect. right bottom"),
+    21: ("Small rect. right main", "Small rect. right top"),
+    22: ("Side line right", "Small rect. right bottom"),
+    23: ("Side line right", "Small rect. right top"),
+    24: ("Goal right crossbar", "Goal right post left"),
+    25: ("Goal right crossbar", "Goal right post right"),
+    26: ("Side line right", "Goal right post left"),
+    27: ("Side line right", "Goal right post right"),
+    28: ("Side line right", "Side line bottom"),
+    29: ("Side line right", "Side line top"),
+}
+
+
+def find_closest_point(points: np.ndarray, point: np.ndarray):
+    distances = np.linalg.norm(points - point, axis=1)
+    closest_index = np.argmin(distances)
+
+    return points[closest_index]
+
+
+def find_intersection(line1: np.ndarray, line2: np.ndarray):
+    while True:
+        x1, y1 = line1[:, 0], line1[:, 1]
+        x2, y2 = line2[:, 0], line2[:, 1]
+
+        x1_mean, x2_mean = np.mean(x1), np.mean(x2)
+
+        x1_vertical = np.all(np.isclose(x1, x1_mean, atol=0.5))
+        x2_vertical = np.all(np.isclose(x2, x2_mean, atol=0.5))
+
+        if x1_vertical:
+            # Two lines must not be vertical at the same time.
+            if x2_vertical:
+                return None
+
+            m2, c2 = P.polyfit(x2, y2, deg=1)
+            x = x1_mean
+            y = m2 * x + c2
+        elif x2_vertical:
+            m1, c1 = P.polyfit(x1, y1, deg=1)
+            x = x2_mean
+            y = m1 * x + c1
+        else:
+            m1, c1 = P.polyfit(x1, y1, deg=1)
+            m2, c2 = P.polyfit(x2, y2, deg=1)
+            x = (c2 - c1) / (m1 - m2) if m1 - m2 != 0 else 0
+            y = m1 * x + c1
+
+        if not line1.shape[0] > 2 and not line2.shape[0] > 2:
+            return x, y
+
+        line1 = find_closest_point(line1, np.array((x, y)))
+        line2 = find_closest_point(line2, np.array((x, y)))
+
+
+def find_ellipse_tangent_intersections(coefficients: tuple, point: np.ndarray):
+    a, b, c, d, e, f = coefficients
+    x1, y1 = point
+
+    A = (
+        4 * a * c * x1 * y1
+        + 2 * a * e * x1
+        - b**2 * x1 * y1
+        - b * d * x1
+        - b * e * y1
+        - 2 * b * f
+        + 2 * c * d * y1
+        + d * e
+    )
+
+    B = 2 * np.sqrt(
+        (-4 * a * c * f + a * e**2 + b**2 * f - b * d * e + c * d**2)
+        * (a * x1**2 + b * x1 * y1 + c * y1**2 + d * x1 + e * y1 + f)
+    )
+
+    C = (
+        4 * a * c * x1**2
+        - b**2 * x1**2
+        - 2 * b * e * x1
+        + 4 * c * d * x1
+        + 4 * c * f
+        - e**2
+    )
+
+    gradients = (A - B) / C, (A + B) / C
+    points = []
+
+    for m in gradients:
+        D = b * m * x1 - b * y1 + 2 * c * m**2 * x1 - 2 * c * m * y1 - d - e * m
+        E = 2 * (a + b * m + c * m**2)
+
+        x = D / E
+        y = y1 + m * x - m * x1
+
+        points.append((x, y))
+
+    return points
+
+
+def find_ellipse_line_intersection(coefficients: tuple):
+    if len(coefficients) < 6:
+        return None
+
+    #
+    pass
+
+
+def get_intersection_points():
+    pass
+
+
+def get_ellipse_points(
+    points: dict[str, np.ndarray],
+    pitch_keypoints: np.ndarray[tuple[Literal[57], Literal[2]]],
+):
+    circles = ["Circle central", "Circle left", "Circle right"]
+
+    circle_points = {
+        "Circle central": {
+            "halfway_line_center_circle_top_mark": {
+                "type": "intersection",
+            },
+            "halfway_line_center_circle_bottom_mark": {
+                "type": "intersection",
+            },
+        },
+        "Circle left": {
+            "top_left_penalty_ark_mark": {
+                "type": "intersection",
+            },
+            "bottom_left_penalty_ark_mark": {
+                "type": "intersection",
+            },
+        },
+    }
+
+    for circle in circles:
+        if circle not in points:
+            continue
+
+        # A minimum of five points is required to define a unique ellipse.
+        # https://sarcasticresonance.wordpress.com/2012/05/14/how-many-points-does-it-take-to-define/
+        if len(points[circle] < 5):
+            continue
+
+        reg = LsqEllipse().fit(points[circle])
+        ellipse = reg.coefficients if len(reg.coefficients) == 6 else None
+
+        if ellipse is None:
+            continue
+
+        if circle == "Circle central":
+            idx1 = KEYPOINTS_INDEX["halfway_top_touch_line_mark"]
+            idx2 = KEYPOINTS_INDEX["center_circle_top_left_tangent"]
+            idx3 = KEYPOINTS_INDEX["center_circle_top_right_tangent"]
+            idx4 = KEYPOINTS_INDEX["halfway_bottom_touch_line_mark"]
+            idx5 = KEYPOINTS_INDEX["center_circle_bottom_left_tangent"]
+            idx6 = KEYPOINTS_INDEX["center_circle_bottom_right_tangent"]
+            idx7 = KEYPOINTS_INDEX["halfway_line_center_circle_top_mark"]
+            idx8 = KEYPOINTS_INDEX["halfway_line_center_circle_bottom_mark"]
+
+            points_left = find_ellipse_tangent_intersections(
+                reg.coefficients,
+                pitch_keypoints[idx1],
+            )
+
+            pitch_keypoints[idx2] = points_left[0]
+            pitch_keypoints[idx3] = points_left[1]
+
+            points_right = find_ellipse_tangent_intersections(
+                reg.coefficients,
+                pitch_keypoints[idx4],
+            )
+
+            pitch_keypoints[idx5] = points_right[0]
+            pitch_keypoints[idx6] = points_right[1]
+
+            # find intersection of the ellipse with the line
+            # points["Middle line"]
+        elif circle == "Circle left":
+            idx1 = KEYPOINTS_INDEX["left_penalty_area_top_right_corner"]
+            idx2 = KEYPOINTS_INDEX["left_penalty_ark_top_tangent"]
+            idx3 = KEYPOINTS_INDEX["left_penalty_area_bottom_right_corner"]
+            idx4 = KEYPOINTS_INDEX["left_penalty_ark_bottom_tangent"]
+
+            points_top = find_ellipse_tangent_intersections(
+                reg.coefficients,
+                pitch_keypoints[idx1],
+            )
+
+            pitch_keypoints[idx2] = points_top[1]
+
+            points_bottom = find_ellipse_tangent_intersections(
+                reg.coefficients,
+                pitch_keypoints[idx3],
+            )
+
+            pitch_keypoints[idx4] = points_bottom[1]
+        elif circle == "Circle right":
+            idx1 = KEYPOINTS_INDEX["right_penalty_area_top_left_corner"]
+            idx2 = KEYPOINTS_INDEX["right_penalty_ark_top_tangent"]
+            idx3 = KEYPOINTS_INDEX["right_penalty_area_bottom_left_corner"]
+            idx4 = KEYPOINTS_INDEX["right_penalty_ark_bottom_tangent"]
+
+            points_top = find_ellipse_tangent_intersections(
+                reg.coefficients,
+                pitch_keypoints[idx1],
+            )
+
+            pitch_keypoints[idx2] = points_top[0]
+
+            points_bottom = find_ellipse_tangent_intersections(
+                reg.coefficients,
+                pitch_keypoints[idx3],
+            )
+
+            pitch_keypoints[idx4] = points_bottom[0]
+
+
+def get_pitch_points():
+    # Get the intersection points of the lines.
+    # Get the ellipse points (TODO: get intersections between circles and lines)
+    # Get homography points
+    pass
+
+
+class Test:
+    @staticmethod
+    def test_find_closest_point():
+        points = np.array([(0, 0), (1, 1), (2, 2)])
+        point = np.array((0, 1))
+
+        closest_point = find_closest_point(points, point)
+
+        assert np.all(closest_point == np.array((0, 0)))
