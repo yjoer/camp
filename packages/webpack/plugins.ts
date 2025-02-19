c7c0778fe20c391f04cd9bf0c05f1bb1e81961c3
Yeoh Joer <yeoh.joer@outlook.com>
2025-02-20 01:57:19 +0800

    feat: hot module replacement on the server side with webpack

---
+++ packages/webpack/plugins.ts
@@ -0,0 +1,31 @@
+/* eslint-disable max-classes-per-file */
+import cp from 'node:child_process';
+import path from 'node:path';
+
+import relocateLoader from '@vercel/webpack-asset-relocator-loader';
+
+import type { Compiler } from 'webpack';
+
+export class AssetRelocatorCachePlugin {
+  apply(compiler: Compiler) {
+    compiler.hooks.compilation.tap('AssetRelocatorCachePlugin', (compilation) => {
+      relocateLoader.initAssetCache(compilation);
+    });
+  }
+}
+
+export class RunScriptPlugin {
+  subprocess?: cp.ChildProcess;
+
+  apply(compiler: Compiler) {
+    compiler.hooks.afterEmit.tap('RunScriptPlugin', () => {
+      if (this.subprocess?.connected) return;
+
+      const { path: outputPath, filename } = compiler.options.output;
+      if (!outputPath) return;
+
+      const entrypoint = path.join(outputPath, filename as string);
+      this.subprocess = cp.fork(entrypoint);
+    });
+  }
+}
