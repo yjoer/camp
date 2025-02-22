cf922f5839055ccd68ff14b09ee8c6a2098a8f8c
Yeoh Joer <yeoh.joer@outlook.com>
2025-02-23 02:12:44 +0800

    chore: add a prettier plugin to break long class names into multiple lines

--- packages/config/prettier.cjs
+++ packages/config/prettier.cjs
@@ -2,11 +2,14 @@
 module.exports = {
   plugins: [
     'prettier-plugin-packagejson',
-    'prettier-plugin-tailwindcss', //
+    'prettier-plugin-tailwindcss',
+    'prettier-plugin-classnames',
+    'prettier-plugin-merge',
   ],
   bracketSameLine: true,
   printWidth: 100,
   quoteProps: 'consistent',
   singleQuote: true,
   tailwindFunctions: ['clsx', 'cva'],
+  experimentalOptimization: true,
 };
