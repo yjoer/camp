20efed434ff3346168f01fcce4ca4edaf012aa65
Yeoh Joer <yeoh.joer@outlook.com>
2025-05-08 00:44:34 +0800

    feat: add the counter example following the elm architecture

---
+++ examples/ratatui/counter-elm/src/main.rs
@@ -0,0 +1,93 @@
+use crossterm::event::{self, Event, KeyCode};
+use ratatui::Frame;
+use ratatui::widgets::Paragraph;
+use std::io;
+use std::time::Duration;
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let mut model = Model::default();
+
+    while model.running_state != RunningState::Done {
+        terminal.draw(|f| view(&model, f))?;
+        let mut msg = handle_events()?;
+
+        while msg.is_some() {
+            msg = update(&mut model, msg.unwrap());
+        }
+    }
+
+    ratatui::restore();
+    Ok(())
+}
+
+fn handle_events() -> io::Result<Option<Message>> {
+    if event::poll(Duration::from_millis(250))? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Right => return Ok(Some(Message::Increment)),
+                    KeyCode::Left => return Ok(Some(Message::Decrement)),
+                    KeyCode::Char('q') => return Ok(Some(Message::Quit)),
+                    _ => return Ok(None),
+                }
+            }
+        }
+    }
+
+    Ok(None)
+}
+
+#[derive(Debug, Default)]
+struct Model {
+    counter: i32,
+    running_state: RunningState,
+}
+
+#[derive(Debug, Default, PartialEq, Eq)]
+enum RunningState {
+    #[default]
+    Running,
+    Done,
+}
+
+enum Message {
+    Increment,
+    Decrement,
+    Reset,
+    Quit,
+}
+
+fn update(model: &mut Model, msg: Message) -> Option<Message> {
+    match msg {
+        Message::Increment => {
+            model.counter += 1;
+
+            if model.counter > 50 {
+                return Some(Message::Reset);
+            }
+        }
+        Message::Decrement => {
+            model.counter -= 1;
+
+            if model.counter < -50 {
+                return Some(Message::Reset);
+            }
+        }
+        Message::Reset => {
+            model.counter = 0;
+        }
+        Message::Quit => {
+            model.running_state = RunningState::Done;
+        }
+    }
+
+    None
+}
+
+fn view(model: &Model, frame: &mut Frame) {
+    frame.render_widget(
+        Paragraph::new(format!("Counter: {}", model.counter)),
+        frame.area(),
+    );
+}
