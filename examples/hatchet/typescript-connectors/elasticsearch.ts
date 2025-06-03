d288cd074b25d2bae8916a3b4d4a44f426aa26c0
Yeoh Joer <yeoh.joer@outlook.com>
2025-06-04 02:04:31 +0800

    feat: reconnect to the cluster after rolling upgrades

--- examples/hatchet/typescript-connectors/elasticsearch.ts
+++ examples/hatchet/typescript-connectors/elasticsearch.ts
@@ -1,7 +1,8 @@
-import { Client } from '@elastic/elasticsearch';
+import { Client, ClusterConnectionPool } from '@elastic/elasticsearch';
 
 export const es = new Client({
   nodes: process.env.ELASTICSEARCH_NODES?.split(','),
+  ConnectionPool: ClusterConnectionPool,
   auth: {
     apiKey: process.env.ELASTICSEARCH_API_KEY ?? '',
   },
