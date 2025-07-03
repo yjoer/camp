5a9fc4eba433958cc05498e441b82f0b56b560ea
Yeoh Joer <yeoh.joer@outlook.com>
2025-07-04 03:57:15 +0800

    feat: add bcrypt hashing endpoint and update rspack configuration

--- examples/webpack/typescript-fastify/rspack.config.ts
+++ examples/webpack/typescript-fastify/rspack.config.ts
@@ -3,8 +3,24 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { getServerConfig } from '@camp/webpack/rspack-config.ts';
+import { rspack } from '@rspack/core';
+import { merge } from 'webpack-merge';
 
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+const ignoredPackages = {
+  'aws-sdk': true,
+  'mock-aws-s3': true,
+  'nock': true,
+};
+
+const customConfig = {
+  plugins: [
+    new rspack.IgnorePlugin({
+      checkResource: (resource) => !!ignoredPackages[resource],
+    }),
+  ],
+};
 
 const config = getServerConfig({
   entry: ['./server.ts'],
@@ -13,4 +29,4 @@ const config = getServerConfig({
   configPath: fileURLToPath(import.meta.url),
 });
 
-export default config;
+export default merge(config, customConfig);
