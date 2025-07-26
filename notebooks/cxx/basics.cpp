ef731e5eb0138a38a721c1f7aeda6a8a94025301
Yeoh Joer <yeoh.joer@outlook.com>
2025-07-26 19:27:05 +0800

    feat: add reverse functionality for linked lists

--- notebooks/cxx/basics.cpp
+++ notebooks/cxx/basics.cpp
@@ -36,6 +36,23 @@ std::cout << "y: " << y++ << std::endl;
 std::cout << "y: " << y << std::endl;
 
 // %% [markdown]
+// ## Declarations
+
+// %% [markdown]
+// ### Arrays
+
+// %%
+int numbers[5] = {1, 2, 3, 4, 5};
+
+for (int i = 0; i < 5; i++)
+  std::cout << numbers[i] << " ";
+std::cout << std::endl;
+
+for (int i = 0; i < 5; i++)
+  std::cout << *(numbers + i) << " ";
+std::cout << std::endl;
+
+// %% [markdown]
 // ## Statements
 
 // %% [markdown]
