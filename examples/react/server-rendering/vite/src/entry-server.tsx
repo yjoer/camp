f2094ba8816b504b6781f17160ab541f589cbaf9
Yeoh Joer <yeoh.joer@outlook.com>
2025-08-31 18:35:16 +0800

    feat: add simple streaming server rendering with vite

--- examples/react/server-rendering/vite/src/entry-server.tsx
+++ examples/react/server-rendering/vite/src/entry-server.tsx
@@ -2,6 +2,12 @@ import ReactDOMServer from 'react-dom/server';
 
 import App from './App';
 
+import type { RenderToPipeableStreamOptions } from 'react-dom/server';
+
 export function render() {
   return ReactDOMServer.renderToString(<App />);
+}
+
+export function renderStream(options: RenderToPipeableStreamOptions) {
+  return ReactDOMServer.renderToPipeableStream(<App />, options);
 }
