e365c80bfa1c0723a0fa65df5be0308dcbf2b7d5
Yeoh Joer <yeoh.joer@outlook.com>
2025-12-03 00:20:27 +0800

    feat: add a javafx todo example implemented with the mvc pattern

---
+++ examples/javafx/todo-mvc/README.md
@@ -0,0 +1,5 @@
+<img
+  src="https://github.com/user-attachments/assets/5cd2c346-ab57-4d8a-82e9-4d770851c164"
+  width="640"
+  height="auto"
+/>
---
+++ examples/javafx/todo-mvc/build.gradle.kts
@@ -0,0 +1,35 @@
+import org.gradle.plugins.ide.eclipse.model.Classpath
+import org.gradle.plugins.ide.eclipse.model.SourceFolder
+
+plugins {
+  application
+  eclipse
+  id("org.openjfx.javafxplugin") version "0.1.0"
+}
+
+repositories { mavenCentral() }
+
+eclipse {
+  classpath {
+    defaultOutputDir = file(".build/eclipse/default")
+    file.whenMerged {
+      val cp = this as Classpath
+      cp.entries.forEach { entry ->
+        if (entry is SourceFolder && entry.output != null) {
+          entry.output = entry.output.replace("bin", ".build/eclipse")
+        }
+      }
+    }
+  }
+}
+
+javafx {
+  version = "23"
+  modules("javafx.controls")
+}
+
+layout.buildDirectory.set(file(".build"))
+
+sourceSets { main { java.srcDirs("src") } }
+
+application { mainClass = "Launcher" }
