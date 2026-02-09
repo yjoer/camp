8b5756e2ed7f3371e5de32980efa58ac0f72f16f
Yeoh Joer <yeoh.joer@outlook.com>
2026-02-09 21:42:14 +0800

    style: wrap long tailwind css classes with eslint

--- stories/src/GradientBorderCard.stories.tsx
+++ stories/src/GradientBorderCard.stories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable better-tailwindcss/no-unknown-classes */
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 const meta = {
@@ -40,16 +41,18 @@ interface GradientBorderCardProps {
 function GradientBorderCard({ image }: GradientBorderCardProps) {
   return (
     <div className="w-64">
-      <div className="relative overflow-hidden rounded-xl border-1 border-white/4">
+      <div className="relative overflow-hidden rounded-xl border border-white/4">
         <img
           alt=""
           className="w-full object-cover duration-150 ease-in hover:scale-105"
           src={image}
         />
         <div
-          className="mask-clip-[content-box,border-box] pointer-events-none absolute inset-0
-            rounded-[11px] bg-[linear-gradient(180deg,rgb(255_255_255/16%),rgb(255_255_255/2%))]
-            mask-[linear-gradient(#fff_0_0),linear-gradient(#fff_0_0)] mask-exclude p-[1px]"></div>
+          className="
+            mask-clip-[content-box,border-box] pointer-events-none absolute inset-0 rounded-[11px]
+            bg-[linear-gradient(180deg,rgb(255_255_255/16%),rgb(255_255_255/2%))]
+            mask-[linear-gradient(#fff_0_0),linear-gradient(#fff_0_0)] mask-exclude p-px
+          "></div>
       </div>
       <h3 className="pt-1 font-semibold text-white/96">Gradient Border Card</h3>
       <p className="line-clamp-1 text-sm text-white/60">
