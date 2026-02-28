4bdaa95e9cd2edb6d3c1e64b0abe984e27aa4d6f
Yeoh Joer <yeoh.joer@outlook.com>
2026-03-01 01:01:48 +0800

    style: use tabs for indentation and automate kotlin style checks

--- examples/javafx/todo-event-bus/build.gradle.kts
+++ examples/javafx/todo-event-bus/build.gradle.kts
@@ -2,30 +2,30 @@ import org.gradle.plugins.ide.eclipse.model.Classpath
 import org.gradle.plugins.ide.eclipse.model.SourceFolder
 
 plugins {
-  application
-  eclipse
-  id("org.openjfx.javafxplugin") version "0.1.0"
+	application
+	eclipse
+	id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 repositories { mavenCentral() }
 
 eclipse {
-  classpath {
-    defaultOutputDir = file(".build/eclipse/default")
-    file.whenMerged {
-      val cp = this as Classpath
-      cp.entries.forEach { entry ->
-        if (entry is SourceFolder && entry.output != null) {
-          entry.output = entry.output.replace("bin", ".build/eclipse")
-        }
-      }
-    }
-  }
+	classpath {
+		defaultOutputDir = file(".build/eclipse/default")
+		file.whenMerged {
+			val cp = this as Classpath
+			cp.entries.forEach { entry ->
+				if (entry is SourceFolder && entry.output != null) {
+					entry.output = entry.output.replace("bin", ".build/eclipse")
+				}
+			}
+		}
+	}
 }
 
 javafx {
-  version = "23"
-  modules("javafx.controls")
+	version = "23"
+	modules("javafx.controls")
 }
 
 dependencies { implementation("com.google.guava:guava:33.5.0-jre") }
