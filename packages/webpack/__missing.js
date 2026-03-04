ed7584a8528ac49c51f83ac2bbbe1ca4a94df57e
Yeoh Joer <yeoh.joer@outlook.com>
2026-03-04 21:38:19 +0800

    chore: add nursery rules and group non-recommended rules

--- packages/webpack/__missing.js
+++ packages/webpack/__missing.js
@@ -1,4 +1,5 @@
 // oxlint-disable import/no-default-export
+// oxlint-disable no-unreachable
 const mod = typeof __resourceQuery === 'string' ? __resourceQuery.slice(1) : 'unknown';
 throw new Error(`optional dependency "${mod}" could not be resolved at build time.`);
 
