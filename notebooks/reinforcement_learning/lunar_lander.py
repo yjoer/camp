fix: use sequence to introduce covariance in its type parameter (#213)

notebooks/reinforcement_learning/lunar_lander.py
@@ -26,6 +26,8 @@ from torch import optim
 
 # %%
 env = gym.make("LunarLander-v3", render_mode="rgb_array")
+observation_space = cast("gym.spaces.Box", env.observation_space)
+action_space = cast("gym.spaces.Discrete", env.action_space)
 
 
 # %%
@@ -66,7 +68,7 @@ class QNetwork(nn.Module):
 
 		self.fc1 = nn.Linear(n_states, 64)
 		self.fc2 = nn.Linear(64, 64)
-		self.fc3 = nn.Linear(64, n_actions)
+		self.fc3 = nn.Linear(64, int(n_actions))
 
 	def forward(self, state: torch.Tensor) -> torch.Tensor:
 		x = torch.relu(self.fc1(state))
@@ -149,8 +151,8 @@ def update_target_network(
 
 
 # %%
-n_states = cast("tuple[int, ...]", env.observation_space.shape)[0]
-n_actions = cast("gym.spaces.Discrete", env.action_space).n
+n_states = observation_space.shape[0]
+n_actions = action_space.n
 n_episodes = 1500
 
 gamma = 0.99

notebooks/reinforcement_learning/taxi.py
@@ -6,11 +6,12 @@ from typing import cast
 import gymnasium as gym
 import imageio
 import numpy as np
-from gymnasium.core import ObsType
 from IPython.display import Image
 
 # %%
 env = gym.make("Taxi-v3", render_mode="rgb_array")
+observation_space = cast("gym.spaces.Discrete", env.observation_space)
+action_space = cast("gym.spaces.Discrete", env.action_space)
 
 
 # %%
@@ -34,7 +35,7 @@ def update_q_table_v2(
 	state: int,
 	action: np.int64,
 	reward: SupportsFloat,
-	next_state: ObsType,
+	next_state: np.int64,
 	alpha: float,
 	gamma: float,
 ) -> None:
@@ -46,7 +47,7 @@ def update_q_table_v2(
 
 # %%
 def get_policy(Q: np.ndarray) -> dict:
-	n_states = env.observation_space.n
+	n_states = observation_space.n
 	return {state: np.argmax(Q[state]) for state in range(n_states)}
 
 
@@ -71,8 +72,8 @@ def simulate_policy(policy: dict) -> list[np.ndarray]:
 
 # %%
 def epsilon_greedy_decayed(n_episodes: int, max_actions: int) -> np.ndarray:
-	n_states = env.observation_space.n
-	n_actions = env.action_space.n
+	n_states = observation_space.n
+	n_actions = action_space.n
 
 	Q = np.zeros((n_states, n_actions))
 